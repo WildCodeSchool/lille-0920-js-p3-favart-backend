18ac64e0b0225882697cc363ad3e51a76601736b
Fx-Colliot <fxcolliot@gmail.com>
2021-01-24 10:59:29 +0100

    Write index.js requests projets, insert database file

--- index.js
+++ index.js
@@ -1,12 +1,84 @@
-const { port } = require("./conf.js")
+const { db, port } = require("./conf.js");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    console.log("GET / was called!")
-    res.send("Hi there!");
+  res.send("Hi there!");
 });
 
-app.listen(port, () => {
-    console.log(`API avalable on http://localhost:${port}`)
-})
+//Get all projets
+app.get("/projet", (req, res) => {
+  db.query("SELECT * from projet", (err, results) => {
+    if (err) {
+      res.status(500).send("Error retrieving data");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+//Post a new projet
+app.post("/projets", (req, res) => {
+  const {
+    titre,
+    porteurs,
+    enjeux,
+    missions,
+    partenaires,
+    territoires,
+    outils,
+  } = req.body;
+  db.query(
+    "INSERT INTO projets(titre,porteurs,enjeux,missions,partenaires,territoires,outils) VALUES (?, ?, ?, ?, ?, ?, ?,)",
+    [titre, porteurs, enjeux, missions, partenaires, territoires, outils],
+    (err, response) => {
+      if (err) {
+        res.status(500).send("Error saving projet");
+      } else {
+        res.status(200).send("Successfully saved !");
+      }
+    }
+  );
+});
+
+//Delete a projet
+app.delete("/projets/:id", (req, res) => {
+  const idProjet = req.params.id;
+  db.query(
+    "DELETE FROM projets WHERE idProjet = ?",
+    [idProjet],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("😱 Error deleting projet");
+      } else {
+        res.status(200).send("🎉 Projet deleted!");
+      }
+    }
+  );
+});
+
+//Update a projet
+app.put("/projets/:id", (req, res) => {
+  // We get the ID from the url:
+  const idProjet = req.params.id;
+  // We get the data from the req.body
+  const newProjet = req.body;
+  // We send a UPDATE query to the DB
+  db.query(
+    "UPDATE projets SET ? WHERE idProjet = ?",
+    [newProjet, idProjet],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error updating projet");
+      } else {
+        res.status(200).send("Projet updated successfully 🎉");
+      }
+    }
+  );
+});
+
+app.listen(port, () => {});
